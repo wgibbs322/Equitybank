4ca114bf413efa0e8513144f9f949050a84e2914
wgibbs322 <wgibbs322@gmail.com>
2025-02-26 00:26:33 -0800

    equity

--- fetchdisplaytransaction.js
+++ fetchdisplaytransaction.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
     try {
-        const response = await fetch('https://ubsbackend.onrender.com/api/transactions');
+        const response = await fetch('https://equitybackend.onrender.com/api/transactions');
         const transactions = await response.json();
 
         if (response.ok) {
